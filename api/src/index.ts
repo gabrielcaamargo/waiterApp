220f5a5564d5f554c9fb63bec1cea520e8af0f4d
Gabriel <gcamargo.dev@gmail.com>
2022-11-16 09:01:32 -0300

    📦 chore: config mongoose and created mongo connection

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 
-const PORT = 3001;
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const PORT = 3001;
 
-app.listen(PORT, () => {
-  console.log(`🔥 Server is running on http://localhost:${PORT}`);
-});
+    app.listen(PORT, () => {
+      console.log(`🔥 Server is running on http://localhost:${PORT}`);
+    });
+
+  })
+  .catch(() => console.log('Error when connecting to Mongo'));
+
