2c16176e1fad3ae0b9fa15096d4d7d2c06dcbbae
Gabriel <gcamargo.dev@gmail.com>
2022-11-16 13:45:39 -0300

    📦 chore: finished all usecases

---
+++ api/src/app/useCases/categories/listProductsByCategories.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import { Product } from '../../models/Product';
+
+export async function listProductsByCategories(req: Request, res: Response) {
+  try {
+    const { categoryId } = req.params;
+    const products = await Product.find().where('category').equals(categoryId);
+
+    res.json(products);
+  }
+
+  catch {
+    res.status(500).json({
+      error: 'Internal server error'
+    });
+  }
+}
