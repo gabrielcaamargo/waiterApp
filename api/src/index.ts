854b2d08493fbc1de712661c281990d46676438f
Gabriel Camargo <gcamargo.dev@gmail.com>
2023-06-15 16:25:46 -0300

    api: mongo connection

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
 
-const PORT = '3001';
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server started at http://localhost:${PORT}`);
-});
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
+    const PORT = '3001';
+    console.log('🚀 Connected to mongo');
+
+    app.listen(PORT, () => console.log(`🚀 Server started at http://localhost:${PORT}`));
+  })
+  .catch(() => console.log('❌ Failed when connecting to mongo'));
+
+
