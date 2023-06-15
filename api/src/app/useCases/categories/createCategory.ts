965e7ccc3eba0849f870d897ba17946eb75e4df0
Gabriel Camargo <gcamargo.dev@gmail.com>
2023-06-15 18:50:34 -0300

    api: create cateories error handling

--- api/src/app/useCases/categories/createCategory.ts
+++ api/src/app/useCases/categories/createCategory.ts
@@ -3,9 +3,13 @@ import { Request, Response } from 'express';
 import { Category } from '../../models/Category';
 
 export async function createCategory(request: Request, response: Response) {
-  const { icon, name } = request.body;
+  try {
+    const { icon, name } = request.body;
 
-  const category = await Category.create({ icon, name });
+    const category = await Category.create({ icon, name });
 
-  response.json(category);
+    response.status(201).json(category);
+  } catch {
+    response.status(500);
+  }
 }
