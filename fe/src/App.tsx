3c95f152aad83ae0705ba44635ad146f6ab08a04
Gabriel Camargo <gcamargo.dev@gmail.com>
2023-06-15 21:31:28 -0300

    frontend: create header

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,3 +1,11 @@
+import { GlobalStyles } from './assets/styles/GlobalStyles';
+import Header from './components/Header';
+
 export default function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <>
+      <GlobalStyles />
+      <Header />
+    </>
+  );
 }
