24d6190085bdd9c396d4cae7c53552559386d073
Gabriel Camargo <gcamargo.dev@gmail.com>
2023-06-15 23:43:09 -0300

    frontend: create order modal

--- fe/src/components/OrdersBoard/index.tsx
+++ fe/src/components/OrdersBoard/index.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import { Order } from '../../types/Order';
+import OrderModal from '../OrderModal';
 import { Board, OrdersContainer } from './styles';
 
 interface OrdersBoardProps {
@@ -8,8 +10,27 @@ interface OrdersBoardProps {
 }
 
 export default function OrdersBoard({icon, title, orders}: OrdersBoardProps) {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedOrder, setSelectedOrder] = useState<Order | null>(null);
+
+  function handleOpenModal(order: Order) {
+    setIsModalVisible(true);
+    setSelectedOrder(order);
+  }
+
+  function handleCloseModal( ) {
+    setIsModalVisible(false);
+    setSelectedOrder(null);
+  }
+
   return (
     <Board>
+      <OrderModal
+        visible={isModalVisible}
+        order={selectedOrder}
+        onClose={handleCloseModal}
+      />
+
       <header>
         <span>{icon}</span>
         <strong>{title}</strong>
@@ -20,7 +41,11 @@ export default function OrdersBoard({icon, title, orders}: OrdersBoardProps) {
         orders.length > 0 && (
           <OrdersContainer>
             {orders.map((order: Order) => (
-              <button type='button' key={order._id}>
+              <button
+                type='button'
+                key={order._id}
+                onClick={() => handleOpenModal(order)}
+              >
                 <strong>Mesa 2</strong>
                 <span>2 itens</span>
               </button>
