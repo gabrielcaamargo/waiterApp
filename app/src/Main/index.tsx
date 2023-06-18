0b3fffcc616b0683361d2a8594667e4f352af776
Gabriel Camargo <gcamargo.dev@gmail.com>
2023-06-18 16:31:30 -0300

    mobile: set safeareaview for any device

---
+++ app/src/Main/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from '../components/Text';
+import { Container } from './styles';
+
+export default function Main() {
+  return (
+    <Container>
+      <Text>Main Renderizou</Text>
+    </Container>
+  );
+}
