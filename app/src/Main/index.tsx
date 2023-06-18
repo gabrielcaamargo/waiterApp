5f9b05d5786bc358c24a2419dc6146890e5c2127
Gabriel Camargo <gcamargo.dev@gmail.com>
2023-06-18 16:35:41 -0300

    mobile: create header

--- app/src/Main/index.tsx
+++ app/src/Main/index.tsx
@@ -1,10 +1,12 @@
 import { Text } from '../components/Text';
 import { Container } from './styles';
 
+import Header from '../components/Header';
+
 export default function Main() {
   return (
     <Container>
-      <Text>Main Renderizou</Text>
+      <Header />
     </Container>
   );
 }
